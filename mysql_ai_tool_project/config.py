9ab0facf102b292c7e50a1959f1c5831c554ce4e
guangxiangdebizi <154864206+guangxiangdebizi@users.noreply.github.com>
2024-11-20 11:07:16 +0800

    Update config.py

--- mysql_ai_tool_project/config.py
+++ mysql_ai_tool_project/config.py
@@ -1,3 +1,3 @@
-API_KEY = "962da72c3cb62b09521923cfefabafc6.oyAzeB72sEIJpWDr"
-BASE_URL = "https://open.bigmodel.cn/api/paas/v4/"
-MODEL_NAME = "GLM-4-FLASH"
+API_KEY = "Your_API_Key"
+BASE_URL = "Your_Base_URL"
+MODEL_NAME = "Model_Name"
