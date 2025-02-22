43a073f00b7413d982de3bd565ad9d4f772ef711
guangxiangdebizi <154864206+guangxiangdebizi@users.noreply.github.com>
2025-02-22 14:27:50 +0800

    Update config.py

--- mysql_ai_tool_project/config.py
+++ mysql_ai_tool_project/config.py
@@ -1,3 +1,3 @@
-API_KEY = "7d02576d2b9c13a647ccc4e34586465a.hlnxicK2PkAmllYl"
-BASE_URL = "https://open.bigmodel.cn/api/paas/v4/"
-MODEL_NAME = "GLM-4-AIR"
+API_KEY = "your api key"
+BASE_URL = "your api url"
+MODEL_NAME = "your model"
